4e8951a0286413008ef61039ea696bda3cc18d79
Guilherme Sette <guilherme.sette@codus.com.br>
2018-12-07 14:21:49 -0200

    Create tag Table and add a colum tags in Posts table

---
+++ rails-base-master/app/helpers/posts_helper.rb
@@ -0,0 +1,3 @@
+module PostsHelper
+  #code
+end
--- rails-base-master/db/schema.rb
+++ rails-base-master/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205132153) do
+ActiveRecord::Schema.define(version: 20181207161638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20181205132153) do
     t.string "title"
     t.text "content"
     t.string "autor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "tags"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
